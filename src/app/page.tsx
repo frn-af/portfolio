076296fc48b8a477c3be5614f021b024200d521c
frn-af <1911512017_farhan@student.unand.ac.id>
2023-11-12 21:57:22 +0700

    Add Info component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import hero from "../assets/images/hero.svg";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
+import { Info } from "@/components/info";
 
 const ttNorm = localFont({
   src: "../assets/fonts/TT-Norms-Pro-ExtraBlack.otf",
@@ -9,19 +10,32 @@ const ttNorm = localFont({
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="absolute p-10">Farhan Alfathra</div>
+    <main className="min-h-screen overflow-visible">
+      <div className="absolute p-10 text-2xl">Farhan Alfathra</div>
+      <div className="absolute w-1/2 -rotate-90 right-24 bottom-1/2">
+        <div className="flex justify-between">
+          <Info color="#424242" variant={"dark"} />
+          <Info color="#424242" variant={"dark"} />
+        </div>
+      </div>
+      <div className="absolute -rotate-90 bottom-1/2 -right-40 ">
+        <div className="flex justify-between">
+          <Info color="#424242" variant={"dark"} />
+          <Info color="#DF505F" variant={"primary"} />
+          <Info color="#1F3982" variant={"secondary"} />
+        </div>
+      </div>
       <div className="flex justify-center items-center h-screen">
         <Image
           src={hero}
           alt="site under construction"
-          width={300}
-          height={300}
+          width={450}
+          height={450}
         />
         <div
           className={cn(
             ttNorm.className,
-            "uppercase absolute text-center text-2xl leading-5 text-[#DF505F]"
+            "uppercase absolute text-center text-4xl leading-9 text-[#DF505F]"
           )}
         >
           <div>cooming</div>
---
+++ src/components/info.tsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const ttNorm = localFont({
+  src: "../assets/fonts/TT-Norms-Pro-Bold.otf",
+});
+
+interface InfoProps {
+  color: string;
+  variant?: VariantProps<typeof infoVariants>["variant"];
+}
+
+const infoVariants = cva("w-60 h-0.5", {
+  variants: {
+    variant: {
+      dark: "bg-[#424242]",
+      primary: "bg-[#DF505F]",
+      secondary: "bg-[#1F3982]",
+    },
+  },
+});
+
+const infoTextVariants = cva("uppercase text-sm leading-3", {
+  variants: {
+    variant: {
+      dark: "text-[#424242]",
+      primary: "text-[#DF505F]",
+      secondary: "text-[#1F3982]",
+    },
+  },
+});
+
+export const Info = ({ color, variant }: InfoProps) => {
+  return (
+    <div>
+      <div className={infoVariants({ variant: variant })}></div>
+      <div className="flex justify-between">
+        <div className={infoTextVariants({ variant: variant })}>
+          <div>site under</div>
+          <div>construction</div>
+        </div>
+        <div
+          className={cn(
+            ttNorm.className,
+            infoTextVariants({ variant: variant }),
+            "text-sm"
+          )}
+        >
+          {color}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// export const Info = ({ color }: InfoProps) => {
+//   return (
+//     <div>
+//       <div className="w-60 h-0.5 bg-[#424242]"></div>
+//       <div className="flex justify-between">
+//         <div className="uppercase text-sm leading-3">
+//           <div>site under</div>
+//           <div>construction</div>
+//         </div>
+//         <div className={cn(ttNorm.className, "text-sm")}>{color}</div>
+//       </div>
+//     </div>
+//   );
+// };
