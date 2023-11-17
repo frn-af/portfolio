902b7d68d3d974937f3c61ecb0393e8551b35cbd
frn-af <1911512017_farhan@student.unand.ac.id>
2023-11-17 20:01:40 +0700

    Add new icons and update layout with meta tags.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#424242" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={cn(
           myFont.className,
