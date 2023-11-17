699bf223d4fbe4b06604760c107461965dffe761
frn-af <1911512017_farhan@student.unand.ac.id>
2023-11-17 20:18:29 +0700

    Update overlay blend mode and add z-index to info boxes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,24 +11,24 @@ const ttNorm = localFont({
 export default function Home() {
   return (
     <main className="h-screen">
-      <div className="absolute z-30 w-full h-screen bg-[url('../assets/images/overlay.svg')] mix-blend-color-burn"></div>
+      <div className="absolute z-30 w-full h-screen bg-[url('../assets/images/overlay.svg')] mix-blend-difference"></div>
       <div className="hidden md:block">
-        <div className="absolute w-60 scale-90 -rotate-90  bottom-0 right-[20%]">
+        <div className="absolute w-60 scale-90 -rotate-90  bottom-0 right-[20%] z-30">
           <Info color="#424242" variant={"dark"} />
         </div>
-        <div className="absolute w-60 scale-90 -rotate-90  bottom-[55%] right-[20%]">
+        <div className="absolute w-60 scale-90 -rotate-90  bottom-[55%] right-[20%] z-30">
           <Info color="#424242" variant={"dark"} />
         </div>
-        <div className="absolute w-60 scale-75  -rotate-90 bottom-[55%] right-36">
+        <div className="absolute w-60 scale-75  -rotate-90 bottom-[55%] right-36 z-30">
           <Info color="#DF505F" variant={"primary"} />
         </div>
-        <div className="absolute w-60 scale-75  -rotate-90  bottom-[25%] right-36">
+        <div className="absolute w-60 scale-75  -rotate-90  bottom-[25%] right-36 z-30">
           <Info color="#424242" variant={"dark"} />
         </div>
-        <div className="absolute w-60 scale-75  -rotate-90  bottom-[85%] right-36">
+        <div className="absolute w-60 scale-75  -rotate-90  bottom-[85%] right-36 z-30">
           <Info color="#1F3982" variant={"secondary"} />
         </div>
-        <div className="absolute w-60 scale-75  -rotate-90  bottom-[90%] right-0">
+        <div className="absolute w-60 scale-75  -rotate-90  bottom-[90%] right-0 z-30">
           <Info color="#DF505F" variant={"primary"} />
         </div>
       </div>
@@ -53,7 +53,7 @@ export default function Home() {
           <div>soon</div>
         </div>
       </div>
-      <div className="absolute w-full px-10 py-5 md:w-[900px] flex justify-end z-10">
+      <div className="absolute w-full px-10 py-5 md:w-[900px] flex justify-end z-30">
         <div className="w-12 h-6 bg-[#424242]"></div>
         <div className="w-12 h-6 bg-[#DF505f]"></div>
         <div className="w-6 h-6 bg-[#1F3982]"></div>
