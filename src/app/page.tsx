2c737aa3156093d8cccf0c4ccaff61a96be07169
frn-af <91farhanalfathra@gmail.com>
2024-04-20 14:58:03 +0700

    feat: update favicon and globalcolor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
   return (
     <main className="max-w-7xl">
-      farhan alfathra
+      <Hero />
     </main>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold">Farhan Alfathra</h1>
+      <h2 className="text-3xl font-semibold">Personal Website</h2>
+    </div>
+  );
+} 
