a833c4c20a3f3212283cb1ad60b870a9abb7351b
frn-af <91farhanalfathra@gmail.com>
2024-04-21 11:42:34 +0700

    feat: ui bento grid layout

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,16 +4,29 @@ import { Card } from "./ui/card";
 
 export default function Hero() {
   return (
-    <div className="flex gap-4 h-full">
-      <div className="p-10">
-        <Image src={logo} alt="Hero" width={70} className="aspect-square" />
-      </div>
-      <Card className="w-full rounded-3xl flex justify-between bg-muted">
-        <Card className="w-1/2 p-10">
-          <h1 className="text-7xl font-bold">Farhan Alfathra</h1>
-          <h1 className="text-7xl font-bold">
-            ⸺ Software Engineer</h1>
-        </Card>
+    <div className="grid grid-cols-4 grid-rows-8 gap-2 h-full p-4">
+      <Card className="col-span-4 bg-orange-500 flex items-center p-4">
+        <Image src={logo} alt="Hero" width={60} className="aspect-square" />
+      </Card>
+      <Card className="bg-muted row-span-7">
+        <h1 className="text-2xl font-bold">Farhan Alfathra</h1>
+        <h1 className="text-2xl font-bold">
+          ⸺ Software Engineer</h1>
+      </Card >
+      <Card className="bg-muted col-span-2">
+        <h1 className="text-2xl font-bold">Farhan Alfathra</h1>
+        <h1 className="text-2xl font-bold">
+          ⸺ Software Engineer</h1>
+      </Card >
+      <Card className="bg-muted row-span-7">
+        <h1 className="text-2xl font-bold">Farhan Alfathra</h1>
+        <h1 className="text-2xl font-bold">
+          ⸺ Software Engineer</h1>
+      </Card >
+      <Card className="bg-muted col-span-2 row-span-6">
+        <h1 className="text-2xl font-bold">Farhan Alfathra</h1>
+        <h1 className="text-2xl font-bold">
+          ⸺ Software Engineer</h1>
       </Card >
     </div >
   );
