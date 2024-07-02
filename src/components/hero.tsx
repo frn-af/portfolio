abc9a5b720e56a7159197805a92096f1fa0d2f2f
frn-af <91farhanalfathra@gmail.com>
2024-07-02 19:48:35 +0700

    ui: outline text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -16,16 +16,18 @@ const Hero = () => {
         </div>
         <div className="space-x-1">
           <Button className="group ml-auto bg-foreground rounded-full border-2 uppercase w-[200px] h-[40px] items-center p-[4px] justify-between">
-            <div className="w-full text-xl uppercase">Project</div>
+            <div className="w-full font-bold text-xl uppercase">Project</div>
             <div className="group-hover:bg-background group-hover:text-foreground bg-primary rounded-full text-sm w-2/3 h-full flex justify-center items-center capitalize">Work</div>
           </Button>
           <Button className="group ml-auto bg-foreground rounded-full border-2 uppercase w-[200px] h-[40px] items-center p-[4px] justify-between">
-            <div className="w-full text-xl uppercase">discover</div>
+            <div className="w-full font-bold text-xl uppercase">discover</div>
             <div className="group-hover:bg-background group-hover:text-foreground bg-primary rounded-full text-sm w-2/3 h-full flex justify-center items-center capitalize">journal</div>
           </Button>
         </div>
       </div>
-      <div>
+      <div className="w-full text-center">
+        <div className="text-[120px] font-heading font-outline-2 text-transparent ">Farhan</div>
+        <div className="text-[100px] font-heading text-black">Alfathra</div>
       </div>
     </div>
   )
